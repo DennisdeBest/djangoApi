f5dbb2332de9ebd4f9144ef1aafa2c07c20151ab
DennisdeBest <dennis@debest.fr>
2017-11-09 18:00:16 +0100

    add route for user delete

--- project/api/ShareTheMatch/urls.py
+++ project/api/ShareTheMatch/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from .views import user
 
 urlpatterns = [
-    url(r'users/$', user.UserCreate.as_view(), name='ShareTheMatch-create'),
-    url(r'users/(\d+)/$', user.UserUpdate.as_view(), name='ShareTheMatch-update'),
+    url(r'users/$', user.STMUser.as_view(), name='ShareTheMatch-create'),
+    url(r'users/(?P<pk>\d+)/',user.STMUser.as_view(), name='ShareTheMatch-delete'),
 ]
--- project/api/ShareTheMatch/views/user.py
+++ project/api/ShareTheMatch/views/user.py
@@ -4,7 +4,8 @@ from rest_framework import status
 from ShareTheMatch.serializers.user import UserSerializer
 from django.contrib.auth.models import User
 
-class UserCreate(APIView):
+
+class STMUser(APIView):
 
     def post(self, request, format='json'):
         serializer = UserSerializer(data=request.data)
@@ -13,11 +14,11 @@ class UserCreate(APIView):
             if user:
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-
-class UserUpdate(APIView):
-    def update(self, request, format='json'):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()
-            if user:
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
+    def delete(self, request, pk):
+        user = request.user
+        if not user.is_authenticated:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        from rest_framework.generics import get_object_or_404
+        user = get_object_or_404(User, pk=pk, user=user)
+        user.delete()
+        return Response(status=status.HTTP_200_OK)
