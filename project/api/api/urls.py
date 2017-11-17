83836feb4f354e3d54ffe12aa3fda2b8a9dfae6f
DennisdeBest <dennis@debest.fr>
2017-11-17 10:20:23 +0100

    Remove api from route

--- project/api/api/urls.py
+++ project/api/api/urls.py
@@ -23,6 +23,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls')),
     url(r'^auth/', views.obtain_auth_token),
-    url(r'^$', hello_world),
-    url(r'^api/', include('ShareTheMatch.urls')),
+    url(r'^', include('ShareTheMatch.urls')),
 ]
