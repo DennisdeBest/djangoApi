9fd3fe1a5448ded5ceb0179526933a26946cc913
DennisdeBest <dennis@debest.fr>
2017-12-15 09:52:23 +0100

    Serve django static assets to backoffice

--- project/api/ShareTheMatch/urls.py
+++ project/api/ShareTheMatch/urls.py
@@ -1,8 +1,10 @@
 from .views import profile, user, adress
 from rest_framework import routers
+from django.conf.urls.static import static
+from django.conf import settings
 
 router = routers.SimpleRouter(trailing_slash=False)
 
 router.register(r'users', user.UserViewSet)
 router.register(r'adresses', adress.AdressViewSet)
-urlpatterns = router.urls
+urlpatterns = router.urls + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
